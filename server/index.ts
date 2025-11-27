4258e56c15fc33e3cca3dfec59fc4b16054c7e3e
Yaroslav Naftulyev <yaroslav.naftulyev@visualping.io>
2025-11-26 19:09:36 -0800

    added initial site

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
-import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { serveStatic } from "./static";
-import { createServer } from "http";
+import express, {type Request, Response, NextFunction} from "express";
+import {registerRoutes} from "./routes";
+import {serveStatic} from "./static";
+import {createServer} from "http";
 
 const app = express();
 const httpServer = createServer(app);
@@ -17,10 +17,10 @@ app.use(
     verify: (req, _res, buf) => {
       req.rawBody = buf;
     },
-  }),
+  })
 );
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 export function log(message: string, source = "express") {
   const formattedTime = new Date().toLocaleTimeString("en-US", {
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
-    res.status(status).json({ message });
+    res.status(status).json({message});
     throw err;
   });
 
@@ -76,15 +76,15 @@ app.use((req, res, next) => {
   if (process.env.NODE_ENV === "production") {
     serveStatic(app);
   } else {
-    const { setupVite } = await import("./vite");
+    const {setupVite} = await import("./vite");
     await setupVite(httpServer, app);
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
+  // Other ports are firewalled. Default to 5005 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+  const port = parseInt(process.env.PORT || "5005", 10);
   httpServer.listen(
     {
       port,
@@ -93,6 +93,6 @@ app.use((req, res, next) => {
     },
     () => {
       log(`serving on port ${port}`);
-    },
+    }
   );
 })();
